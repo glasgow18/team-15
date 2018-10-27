1ddcd7b024aafd77f5e897d148e63dbbb96d006b
Joshua Richardson <joshua@richardson.tech>
2018-10-27 15:50:13 +0100

    Adds django tutorial

---
+++ discovery/discovery_frontend/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+
+from discovery_frontend import views
+
+urlpatterns = [
+    url("/hello", views.hello)
+]
--- discovery/discovery_frontend/views.py
+++ discovery/discovery_frontend/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def hello(req):
+    # some random code to search for stuff
+    search_result = "some result"
+    return render(req, "inherits.html", context={'result': search_result})
