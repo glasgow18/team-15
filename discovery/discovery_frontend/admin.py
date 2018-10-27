cdeec6058b2666411d80d02c5f1c2133171f0cff
Joshua Richardson <joshua@richardson.tech>
2018-10-27 16:12:49 +0100

    Adds example model and migration and query and renderer

--- discovery/discovery_frontend/admin.py
+++ discovery/discovery_frontend/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from discovery_frontend.models import Location
+
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Location, LocationAdmin)
--- discovery/discovery_frontend/models.py
+++ discovery/discovery_frontend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    free = models.BooleanField(default=True)
+    price = models.IntegerField()
+    description = models.CharField(max_length=1000)
