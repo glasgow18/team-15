0aaad809bab4cda2fbe943eb440503c487e26234
Lasse <lasse@go4more.de>
2018-10-27 19:55:31 +0100

    +added django form to modal dialog

--- discovery/discovery_frontend/views.py
+++ discovery/discovery_frontend/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from discovery_frontend.forms import AddLocationForm
 from discovery_frontend.models import Location
 
 
 def hello(req):
     # some random code to search for stuff
     search_result = Location.objects.all()
-    return render(req, "inherits.html", context={'result': search_result})
+    add_location_form = AddLocationForm()
+
+    return render(req, "inherits.html", context={'form': add_location_form})
